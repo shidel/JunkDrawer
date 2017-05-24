f4ad3f432455063f0041ba38daf243bc9505e5ef
Shidel <shidel@users.noreply.github.com>
2017-05-24 09:08:08 -0400

    swapper script

--- bash/swapper.sh
+++ bash/swapper.sh
@@ -4,11 +4,11 @@ declare -r SOURCE_CHARS='ABCD123'
 declare -r DEST_CHARS='321BCDA'
 
 function swapChars () {
-	local line="$@"
-	local outline=""
-	local charpos=0
-	local char
-	local pos
+    local line="$@"
+    local outline=""
+    local charpos=0
+    local char
+    local pos
     while [[ $charpos -lt ${#line} ]] ; do
         char="${line:$charpos:1}"
         pos="${SOURCE_CHARS%%$char*}"
